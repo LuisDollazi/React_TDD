1de37a3e40d600cd2802329092eb014982ea21ce
Luis Roberto Moreno Dollazi <luis.moreno.dol@gmail.com>
2025-04-24 16:03:22 -0300

    initial creation and start of study of jest

---
+++ src/__test__/example_globals.test.js
@@ -0,0 +1,21 @@
+test('description', () => {
+    expect(true).toBe(true)
+})
+
+test('description 2', () => {
+    expect(true).toBe(true)
+})
+
+test('description 3', () => {
+    expect(true).toBe(true)
+})
+
+describe('this is for group test cases', () => {
+    it('description 4', () => {
+        expect(true).toBe(true)
+    })
+    
+    it('description 5', () => {
+        expect(true).toBe(true)
+    })
+})
